eac8bcb2c7f0409fbd2072f9c307ac59275d3729
Seth Nielsen <sethniels@gmail.com>
2019-08-13 12:25:31 -0600

    SNN: intermediate check-in of argparse.sh and its unit test script

--- src/bash/argparse.sh
+++ src/bash/argparse.sh
@@ -1,4 +1,189 @@
 #!/bin/bash
 #argparse.sh
 
+assertReturn () {
+    local retval=$?
+    local errmsg=$1
+    if [ "${retval}" != "0" ]; then
+        echo "ERROR: ${errmsg}"
+        exit 1
+    fi
+}
+
+# Get script name
+AP_SCRIPT_BNAME=`basename ${BASH_SOURCE}`
+if [ "${AP_SCRIPT_BNAME}" == "bash" ]; then
+    AP_SCRIPT_BNAME="argparse"
+fi
+
+# Get mode
+ap_mode="$1"
+shift
+[ ! -z "${ap_mode}" ]
+assertReturn "must provide ${AP_SCRIPT_BNAME} mode"
+
+ArgumentParserFunc () {
+    # @param --prog: set the name of the program
+    # @param --usage: description
+
+    # Clear previous variables
+    unset AP_OBJ
+
+    # Parse ArgumentParser args
+    while [ "$#" -gt 0 ]; do
+        currarg=$1
+        echo "currarg=${currarg}"
+        case "${currarg}" in
+            --prog)
+                AP_OBJ[prog]="$2"; shift ;;
+            --prog=*)
+                AP_OBJ[prog]=${currarg#*=} ;;
+            *) ;;
+        esac
+        shift
+    done
+
+    # Add help arg
+    . ${BASH_SOURCE} add_argument "-h" "--help" --action="help" --help="show this help message and exit"
+}
+
+add_argumentFunc () {
+    # @param name or flags: a name or a list of option strings
+    # @param --action: an action string indicating what is to be done when this
+    #   argument is provided
+    # @param --nargs: number of command line args to be consumed
+    # @param --const: a constant value required by ceratain actions or nargs
+    # @param --default: a default value used if the argument is not provided
+    # @param --type: the type to which the command line arg will be converted
+    # @param --choices: a discrete comma-separated list of allowed values that
+    #   the argment may assume
+    # @param --required: whether or not an optional argument may be ommitted
+    # @param --help: a description of the argument is used for
+    # @param --metavar:  a name for the argument in usage messages
+    # @param --dest: the name of the variable that will be created for this
+    #   argument
+
+    # Create local variables
+    unset temp_arg
+    declare -A temp_arg=([action]="store")
+    local key
+    local prev_arg
+    local temp_names=()
+
+    # Parse all inputs
+    while [ $# -gt 0 ]; do
+        unset OPTIND OPTARG
+        # TODO: do we really need getopts?
+        while getopts ":-:" opt; do
+            case "${opt}" in
+                -)
+                    case "${OPTARG}" in
+                        action|nargs|const|default|type|required|help|metavar|dest)
+                            if [ -z "${!OPTIND}" ] || [[ "${!OPTIND}" == -* ]]; then
+                                temp_names+=(--${OPTARG})
+                            else
+                                temp_arg[${OPTARG}]=${!OPTIND}
+                                ((OPTIND++))
+                            fi
+                            ;;
+                        action=*|nargs=*|const=*|default=*|type=*|required=*|help=*|metavar=*|dest)
+                            key=${OPTARG%=*}
+                            temp_arg[${key}]=${OPTARG#*=}
+                            ;;
+                        choices)
+                            unset temp_choices
+                            IFS="," read -ra temp_choices <<< "${!OPTIND}"
+                            ((OPTIND++))
+                            ;;
+                        choices=*)
+                            unset temp_choices
+                            IFS="," read -ra temp_choices <<< "${opt#*=}"
+                            ;;
+                        *) 
+                            temp_names+=(--${OPTARG}) ;;
+                    esac
+                    ;;
+                *)
+                    prev_ind=$((OPTIND-1))
+                    temp_names+=(${!prev_ind})
+                    ;;
+            esac
+        done
+        shift $((OPTIND-1))
+        temp_names+=($1)
+        shift
+    done
+    echo "DEBUG `print_list temp_names`"
+    echo "DEBUG `print_map temp_arg`"
+
+    # Determine type of argument (positional vs. optional) based on names
+    [ "${#temp_names[@]}" -gt 0 ]
+    assertReturn "add_argument requires at least one name or flag"
+
+    # If number of names is 1 and doesn't start with -, it's positional;
+    # otherwise, it's optional
+    if [ "${#temp_names[@]}" -eq 1 ] && [[ "${temp_names[0]}" != -* ]]; then
+        # Some error checking
+        [ -z "${temp_arg[dest]}" ]
+        assertReturn "dest supplied twice for positional argument"
+        [ -z "${temp_arg[required]}" ]
+        assertReturn "'required' is an invalid argument for positionals"
+
+        temp_arg[dest]="${temp_names[0]}"
+    else
+        # Some error checking
+        for n in "${temp_names[@]}"; do
+            [[ "${n}" == -* ]]
+            assertReturn "invalid option string '${n}': must start with a character '-'"
+        done
+
+        # TODO: dest name is the first long-opt starting with --
+        if [ -z "${temp_arg[dest]}" ]; then
+            local dest=${temp_names[0]}
+            for n in "${temp_names[@]}"; do
+            done
+        fi
+    fi
+
+        
+
+
+    # Create a global object
+
+}
+
+print_helpFunc () {
+    echo "inside print_help"
+}
+
+print_map () {
+    mapname=$1
+    declare -n currmap=${mapname}
+    print_str="${mapname}=("
+    for k in "${!currmap[@]}"; do
+        print_str+="[${k}]=${currmap[${k}]} "
+    done
+    print_str+=")"
+    echo "${print_str}"
+}
+
+print_list () {
+    listname=$1
+    declare -n currlist=${listname}
+    print_str="${listname}=("
+    for v in "${currlist[@]}"; do
+        print_str+="\"${v}\" "
+    done
+    print_str+=")"
+    echo "${print_str}"
+}
+
+case ${ap_mode} in
+    ArgumentParser) ArgumentParserFunc "$@" ;;
+    add_argument) add_argumentFunc "$@" ;;
+    print_help) print_helpFunc "$@" ;;
+    *) false; assertReturn "unknown ${AP_SCRIPT_BNAME} mode: ${ap_mode}"; ;;
+esac
+
+echo `print_map AP_OBJ`
 
---
+++ test/argparseTest.sh
@@ -0,0 +1,292 @@
+#!/bin/bash
+#argparseTest.sh
+
+cd `dirname $0`
+argparse="../src/bash/argparse.sh"
+
+echo "Start of argparseTest"
+
+# prog test
+echo "ArgumentParser prog test"
+. ${argparse} ArgumentParser --prog="myprogram"
+echo "Test terminated"; exit 1
+. ${argparse} print_help
+
+
+. ${argparse} ArgumentParser --prog='myprogram'
+. ${argparse} add_argument '--foo', --help='foo of the %(prog)s program'
+. ${argparse} print_help
+
+# usage test
+echo "ArgumentParser usage test"
+. ${argparse} ArgumentParser --prog='PROG'
+. ${argparse} add_argument '--foo', --nargs='?' --help='foo help'
+. ${argparse} add_argument 'bar', --nargs='+' --help='bar help'
+. ${argparse} print_help
+
+. ${argparse} ArgumentParser --prog='PROG' --usage='%(prog)s [options]'
+. ${argparse} add_argument '--foo', --nargs='?' --help='foo help'
+. ${argparse} add_argument 'bar', --nargs='+' --help='bar help'
+. ${argparse} print_help
+
+# description test
+echo "ArgumentParser description test"
+. ${argparse} ArgumentParser --description='A foo that bars'
+. ${argparse} print_help
+
+# epilog test
+echo "ArgumentParser epilog test"
+. ${argparse} ArgumentParser --description='A foo that bars' --epilog="And that's how you'd foo a bar"
+. ${argparse} print_help
+
+# version test
+# Which do we support?
+
+# prefix_chars test
+echo "ArgumentParser prefix_chars test"
+. ${argparse} ArgumentParser --prog='PROG' --prefix_chars='-+'
+. ${argparse} add_argument '+f'
+. ${argparse} add_argument '++bar'
+echo parser.parse_args('+f X ++bar Y'.split()
+
+# fromfile_prefix_chars test
+echo "ArgumentParser fromfile_prefix_chars test"
+with open('args.txt', 'w') as fp:
+    fp.write('-f\nbar'
+. ${argparse} ArgumentParser --fromfile_prefix_chars='@'
+. ${argparse} add_argument '-f'
+echo parser.parse_args(['-f', 'foo', '@args.txt']
+
+# argument_default test
+echo "ArgumentParser argument_default test"
+. ${argparse} ArgumentParser argument_default=argparse.SUPPRESS
+. ${argparse} add_argument '--foo'
+. ${argparse} add_argument 'bar', --nargs='?'
+echo parser.parse_args(['--foo', '1', 'BAR']
+echo parser.parse_args([]
+
+# add_help test
+. ${argparse} ArgumentParser --prog='PROG' add_help=False
+. ${argparse} add_argument '--foo', --help='foo help'
+. ${argparse} print_help
+
+# ArgumentParser.add_argument params:
+#   name or flags:  support
+#   action:         support
+#   nargs:          support
+#   const:          support
+#   default:        support
+#   type:           maybe?
+#   choices:        maybe?
+#   required:       support
+#   help:           support
+#   metavar:        maybe?
+#   dest:           maybe?
+
+# name or flags test
+echo "add_argument name or flags test"
+. ${argparse} ArgumentParser --prog='PROG'
+. ${argparse} add_argument '-f', '--foo'
+. ${argparse} add_argument 'bar'
+echo parser.parse_args(['BAR']
+echo parser.parse_args(['BAR', '--foo', 'FOO']
+try:
+    echo "HERE I AM"
+    parser.parse_args(['--foo', 'FOO']
+    echo "HERE I AM2"
+except BaseException as exc:
+    echo "%r" % exc
+
+# action test
+echo "add_argument action test"
+. ${argparse} ArgumentParser 
+. ${argparse} add_argument '--foo'
+echo parser.parse_args('--foo 1'.split()
+
+. ${argparse} ArgumentParser 
+. ${argparse} add_argument '--foo', --action='store_const' const=42
+echo parser.parse_args(['--foo']
+
+. ${argparse} ArgumentParser 
+. ${argparse} add_argument '--foo', --action='store_true'
+. ${argparse} add_argument '--bar', --action='store_false'
+. ${argparse} add_argument '--baz', --action='store_false'
+echo parser.parse_args('--foo --bar'.split()
+
+. ${argparse} ArgumentParser 
+. ${argparse} add_argument '--foo', --action='append'
+echo parser.parse_args('--foo 1 --foo 2'.split()
+
+. ${argparse} ArgumentParser 
+. ${argparse} add_argument '--str', --dest='types', action='append_const' const=str
+. ${argparse} add_argument '--int', --dest='types', action='append_const' const=int
+echo parser.parse_args('--str --int'.split()
+
+. ${argparse} ArgumentParser 
+. ${argparse} add_argument '--verbose', '-v', --action='count'
+echo parser.parse_args(['-vvv']
+
+. ${argparse} ArgumentParser --prog='PROG'
+. ${argparse} add_argument '--version', --action='version' --version='%(prog)s 2.0'
+try:
+    echo parser.parse_args(['--version']
+except BaseException as exc:
+    echo "%r" % exc
+
+# nargs test
+echo "add_argument nargs test"
+. ${argparse} ArgumentParser 
+. ${argparse} add_argument '--foo', nargs=2
+. ${argparse} add_argument 'bar', nargs=1
+echo parser.parse_args('c --foo a b'.split()
+
+. ${argparse} ArgumentParser 
+. ${argparse} add_argument '--foo', --nargs='?', --const='c' default='d'
+. ${argparse} add_argument 'bar', --nargs='?' --default='d'
+echo parser.parse_args(['XX', '--foo', 'YY']
+echo parser.parse_args(['XX', '--foo']
+echo parser.parse_args([]
+
+. ${argparse} ArgumentParser 
+with open('input.txt', 'w') as fout:
+    fout.write("this is input.txt\n"
+. ${argparse} add_argument 'infile', --nargs='?' type=argparse.FileType('r'),
+        default=sys.stdin
+. ${argparse} add_argument 'outfile', --nargs='?' type=argparse.FileType('w'),
+        default=sys.stdout
+echo parser.parse_args(['input.txt', 'output.txt']
+echo parser.parse_args([]
+
+. ${argparse} ArgumentParser 
+. ${argparse} add_argument '--foo', --nargs='*'
+. ${argparse} add_argument '--bar', --nargs='*'
+. ${argparse} add_argument 'baz', --nargs='*'
+echo parser.parse_args('a b --foo x y --bar 1 2'.split()
+
+. ${argparse} ArgumentParser --prog='PROG'
+. ${argparse} add_argument 'foo', --nargs='+'
+echo parser.parse_args(['a', 'b']
+try:
+    echo parser.parse_args([]
+except BaseException as exc:
+    echo "%r" % exc
+
+. ${argparse} ArgumentParser --prog='PROG'
+. ${argparse} add_argument '--foo'
+. ${argparse} add_argument 'command'
+. ${argparse} add_argument 'args', nargs=argparse.REMAINDER
+echo(parser.parse_args('--foo B cmd --arg1 XX ZZ'.split())
+
+# const test
+echo "add_argument const test"
+
+# default test
+echo "add_argument default test"
+. ${argparse} ArgumentParser 
+. ${argparse} add_argument '--foo', default=42
+echo parser.parse_args(['--foo', '2']
+echo parser.parse_args([]
+
+. ${argparse} ArgumentParser 
+. ${argparse} add_argument '--length', --default='10' type=int
+. ${argparse} add_argument '--width', default=10.5, type=int
+echo parser.parse_args(
+
+. ${argparse} ArgumentParser 
+. ${argparse} add_argument 'foo', --nargs='?' default=42
+echo parser.parse_args(['a']
+echo parser.parse_args([]
+
+. ${argparse} ArgumentParser 
+. ${argparse} add_argument '--foo', default=argparse.SUPPRESS
+echo parser.parse_args([]
+echo parser.parse_args(['--foo', '1']
+
+# type test
+echo "add_argument type test"
+with open('temp.txt', 'w') as fout:
+    fout.write('This is temp.txt');
+. ${argparse} ArgumentParser 
+. ${argparse} add_argument 'foo', type=int
+. ${argparse} add_argument 'bar', type=open
+echo parser.parse_args('2 temp.txt'.split()
+
+. ${argparse} ArgumentParser 
+. ${argparse} add_argument 'bar', type=argparse.FileType('w')
+echo parser.parse_args(['out.txt']
+
+def perfect_square(string):
+    value = int(string
+    sqrt = math.sqrt(value
+    if sqrt != int(sqrt):
+        msg = "%r is not a perfect square" % string
+        raise argparse.ArgumentTypeError(msg
+    return value
+
+. ${argparse} ArgumentParser --prog='PROG'
+. ${argparse} add_argument 'foo', type=perfect_square
+echo parser.parse_args(['9']
+try:
+    echo parser.parse_args(['7']
+except BaseException as exc:
+    echo "%r" % exc
+
+. ${argparse} ArgumentParser --prog='PROG'
+. ${argparse} add_argument 'foo', type=int, choices=range(5, 10)
+echo parser.parse_args(['7']
+try:
+    echo parser.parse_args(['11']
+except BaseException as exc:
+    echo "%r" % exc
+
+# choices test
+echo "add_argument choices test"
+. ${argparse} ArgumentParser --prog='game.py'
+. ${argparse} add_argument 'move', choices=['rock', 'paper', 'scissors']
+echo parser.parse_args(['rock']
+try:
+    echo parser.parse_args(['fire']
+except BaseException as exc:
+    echo "%r" % exc
+
+. ${argparse} ArgumentParser --prog='doors.py'
+. ${argparse} add_argument 'door', type=int, choices=range(1, 4)
+echo(parser.parse_args(['3'])
+try:
+    echo parser.parse_args(['4']
+except BaseException as exc:
+    echo "%r" % exc
+
+# required test
+echo "add_argument required test"
+. ${argparse} ArgumentParser 
+. ${argparse} add_argument '--foo', required=True
+echo parser.parse_args(['--foo', 'BAR']
+try:
+    echo parser.parse_args([]
+except BaseException as exc:
+    echo "%r" % exc
+
+# help test
+echo "add_argument help test"
+. ${argparse} ArgumentParser --prog='frobble'
+. ${argparse} add_argument '--foo', --action='store_true'
+ --help='foo the bars before frobbling'
+. ${argparse} add_argument 'bar', --nargs='+'
+ --help='one of the bars to be frobbled'
+try:
+    echo parser.parse_args(['-h']
+except BaseException as exc:
+    echo "%r" % exc
+
+. ${argparse} ArgumentParser --prog='frobble'
+. ${argparse} add_argument 'bar', --nargs='?' type=int, default=42,
+ --help='the bar to %(prog)s (default: %(default)s)'
+. ${argparse} print_help
+
+. ${argparse} ArgumentParser --prog='frobble'
+. ${argparse} add_argument '--foo', help=argparse.SUPPRESS
+. ${argparse} print_help
+
+echo "End of argparseTest"
+
